a9ce75b21ec2d91ce7d73e484f8920bdf9127b9a
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-01-10 12:21:20 +0100

    added the correct props and made it so the jobb card can take json input for the time being

--- frontend/src/components/JobCard.vue
+++ frontend/src/components/JobCard.vue
@@ -1,29 +1,33 @@
-<script setup>
-import { ref } from 'vue'
-let companyName = ref("Intility AS")
-let possiton = ref("IT-drift")
-let location = ref("OSLO")
-let applicationDeadline = ref("20.01.2023")
-let datePosted = ref("10.01.2023")
-let number_of_positions = ref("2 ")
-
-
-
-
-
-</script>
 <template>
   <div class="card w-85 bg-base-300 shadow-xl mt-5 mb-5 mx-5">
   <div class="card-body">
     <h2 class="card-title">{{ companyName }}</h2>
-    <p>Fag: {{ possiton }}</p>
+    <p>Fag: {{ position }}</p>
     <p>Lokasjon: {{ location }}</p>
     <p>Søknads frist: {{ applicationDeadline }}</p>
-    <p>Dato lagt ut: {{datePosted}}</p>
-    <p>Antal Stillinger: 2</p>
+    <p>Dato lagt ut: {{ datePosted }}</p>
+    <p>Antal Stillinger: {{ numberOfPositions }}</p>
     <div class="card-actions justify-center">
-      <button class="btn btn-primary"><a href="http://"></a>Link til stillingsannonse</button>
+      <button class="btn btn-primary"><a :href="JobAdLink">Link til stillingsannonse</a></button>
     </div>
   </div>
 </div>
 </template>
+
+<script>
+
+export default {
+  props: {
+    jobId: Number,
+    companyName: String,
+    position: String,
+    location: String,
+    applicationDeadline: String,
+    datePosted: String,
+    numberOfPositions: Number,
+    JobAdLink: String
+  },
+};
+
+
+</script>
---
+++ frontend/src/views/DriftView.vue
@@ -0,0 +1,36 @@
+<script>
+import JobCard from '../components/JobCard.vue'
+import jobListings from '../assets/jobListings.json'
+export default {
+  components: {
+    JobCard,
+  },
+  data() {
+    return {
+      jobList: jobListings
+    };
+  },
+};
+</script>
+
+
+<template>
+     <div class="justify-center text-center">
+    <h1 class="font-bold">Dette er IT-Drift</h1>
+  </div>
+    
+    <div class="justify-center grid grid-flow-row-dense grid-cols-auto grid-rows-auto  md:grid-cols-3 md:grid-rows-3  mb-10 mt-15" >
+      <JobCard
+      v-for="job in jobList"
+      :key="job.jobId"
+      :jobId="job.jobId"
+      :companyName="job.companyName"
+      :position="job.position"
+      :location="job.location"
+      :applicationDeadline="job.applicationDeadline"
+      :datePosted="job.datePosted"
+      :numberOfPositions="job.numberOfPositions"
+      :JobAdLink="job.JobAdLink"
+    />
+    </div>
+</template>
