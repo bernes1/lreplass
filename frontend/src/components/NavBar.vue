f7beeb6a12686e9fb133e63ff5263c5fafb6fbab
Sandra Elise Eng <sandra.eng@live.no>
2024-02-28 13:19:13 +0100

    Fixed navbar component

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,16 +1,15 @@
 <template>
-<div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">læreplas</a>
+  <div class="navbar bg-base-100">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">læreplass</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/drift">IT-Drift</RouterLink></li>
+        <li><RouterLink to="/utvikling">IT-Utvikling</RouterLink></li>
+        <li><RouterLink to="/about">About</RouterLink></li>
+      </ul>
+    </div>
   </div>
-  <div class="flex-none">
-    <ul class="menu menu-horizontal px-1">
-      <li><a>IT-Drift</a></li>
-      <li><a>IT-Utvikling</a></li>
-      <li><RouterLink to="/">Home</RouterLink></li>
-      <li><RouterLink to="/about">About</RouterLink></li>
-    </ul>
-  </div>
-</div>
 </template>
-    
