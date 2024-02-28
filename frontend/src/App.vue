af7b0ccca1fde93a28cf2d04eedcdaa3341f9409
Sandra Elise Eng <117898702+Zandra7@users.noreply.github.com>
2024-02-28 13:23:33 +0100

    Fixed navbar component (#5)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import navbar from '@/components/NavBar.vue';
 </script>
 
 <template>
   <header>
-    <div class="navbar bg-base-100">
-      <div class="flex-1">
-        <a class="btn btn-ghost text-xl">læreplas</a>
-      </div>
-      <div class="flex-none">
-        <ul class="menu menu-horizontal px-1">
-          <li><RouterLink to="/">Home</RouterLink></li>
-          <li><RouterLink to="/drift">IT-Drift</RouterLink></li>
-          <li><RouterLink to="/utvikling">IT-Utvikling</RouterLink></li>
-          <li><RouterLink to="/about">About</RouterLink></li>
-        </ul>
-      </div>
-    </div>
+    <navbar></navbar>
   </header>
 
 <RouterView /></template>
