b3bb9da3e4eae7bae1996a99a76975dd3363b335
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-06-06 14:20:29 +0200

    chore: fixed path issue with dotenv

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,8 +1,20 @@
 import pg from 'pg'
-import dotenv from 'dotenv'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { resolve } from 'path';
+import dotenv from 'dotenv';
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Resolve the path to two directories up and then into 'vars/.env'
+const envPath = resolve(__dirname, '../../vars/.env');
+
+//decaling that i want to use the pool module from pg
 const { Pool } = pg
 
-dotenv.config({path: '../../vars/.env'})
+dotenv.config({path: envPath})
 
 export const pool = new Pool({
   user: process.env.POSTGRES_USER, 
