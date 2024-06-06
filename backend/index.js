685aa3cace8f53a1c3bbd419920cbd366e46866f
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-06-06 14:19:42 +0200

    chore: added query options

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,39 @@
 import express from 'express';
 import * as db from './db/index.js'
-    
+import cors from 'cors'
+
 const app = express();
 const port = 3000;
 
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
 app.get('/api/v1/utvikling', (req, res) => {
-    res.send('utvikling');
+  const position = 'IT-Utvikling';
+  db.pool.query('SELECT * FROM job_listings WHERE position = $1',[position] ,(err, result) => {
+    if (err) {
+      console.error('Error executing query', err.stack);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json(result.rows);
+  });
 });
 
 app.get('/api/v1/drift', (req, res) => {
-    res.send('drift');
+  const position = 'IT-Drift';
+  db.pool.query('SELECT * FROM job_listings WHERE position = $1',[position] ,(err, result) => {
+    if (err) {
+      console.error('Error executing query', err.stack);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+    res.status(200).json(result.rows);
+  });
 });
+
+
 
 
 app.listen(port, () => {
