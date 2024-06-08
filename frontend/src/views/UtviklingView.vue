1a2d53947e358a6dedccc6c3b33159c897d73dcf
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-06-08 20:30:28 +0200

    chore: rmeoved uneccary code

--- frontend/src/views/UtviklingView.vue
+++ frontend/src/views/UtviklingView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen">
     <div class="justify-center text-center">
-      <h1 class="font-bold text-2xl">IT-Drift</h1>
+      <h1 class="font-bold text-2xl">IT-Utvikling</h1>
     </div>
     <div class="justify-center grid grid-flow-row-dense grid-cols-auto grid-rows-auto md:grid-cols-3 md:grid-rows-3 mb-10 mt-15">
       <JobCard
@@ -56,32 +56,3 @@ export default {
   }
 };
 </script>
-
-
-<template>
-  <div class="h-screen">
-  
-  <div class="justify-center text-center">
-    <h1 class="font-bold text-2xl">IT-Utvikling</h1>
-  </div>
-  <div
-    class="justify-center grid grid-flow-row-dense grid-cols-auto grid-rows-auto  md:grid-cols-3 md:grid-rows-3  mb-10 mt-15">
-    <JobCard
-      v-for="job in jobList"
-      :key="job.jobId"
-      :jobId="job.jobId"
-      :companyName="job.companyName"
-      :position="job.position"
-      :location="job.location"
-      :applicationDeadline="job.applicationDeadline"
-      :datePosted="job.datePosted"
-      :numberOfPositions="job.numberOfPositions"
-      :JobAdLink="job.JobAdLink"
-    />
-  </div>
-  </div>
-<Footer></Footer>
-  </div>
-</template>
-
-
