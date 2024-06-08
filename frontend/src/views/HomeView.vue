8b51466e66682a21f077131da2862054112cfcc2
bernes1 <37117219+bernes1@users.noreply.github.com>
2024-06-08 20:35:24 +0200

    chore: updated the homepage to have som text

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,9 +4,19 @@ import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
-  <main>
+  <main class="h-screen">
     <div class="text-center">
-<h1>home page</h1>
+      <h1 class="font-bold text-2xl">Velkomen til Lreplass</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis beatae reprehenderit officia reiciendis inventore ipsam blanditiis soluta, nemo qui quasi architecto tenetur, eligendi aspernatur et nesciunt accusantium! Quisquam, deserunt nisi?
+      Error blanditiis provident voluptatem magnam autem, ipsam doloremque, minima aliquam facere inventore, assumenda omnis. Eius ab pariatur reiciendis aperiam vitae. Debitis recusandae itaque obcaecati soluta, ipsa eius quae corporis minus.
+      Modi quibusdam culpa minima, velit fugiat excepturi possimus dolor optio libero incidunt ducimus labore distinctio. Et consectetur excepturi ipsum beatae sit esse ab eveniet repellat? Iste dolor ipsa neque at!
+      Quasi commodi aperiam fuga quae quod dolore? Adipisci inventore aliquid quo odit, quia fugiat reprehenderit atque accusantium nobis! Hic aliquam ut tenetur sequi eligendi! Doloremque necessitatibus reprehenderit doloribus ratione amet!
+      Suscipit blanditiis dolorum nihil? Cum mollitia quisquam explicabo blanditiis deleniti doloremque sint ullam quae earum ad odio animi repellendus atque ex eaque, eveniet nesciunt dolorem veniam cumque molestias optio magni.
+      Fuga aliquam sunt expedita beatae, rerum cum nihil, possimus ea dolor laboriosam molestias dignissimos. Maxime, vero in, sapiente sit neque dolore quos velit exercitationem repellendus obcaecati id dolor rerum officiis.
+      Dolorem libero, voluptatem sunt natus nam facilis quidem maxime aliquid fuga, quae vitae voluptate dicta ipsam illo repudiandae modi obcaecati? Cupiditate voluptate dolorem maxime veritatis perferendis tempora quae reiciendis magnam?
+      Voluptatem iure sapiente nisi, aut laborum doloremque corporis animi ea maxime et dolor repudiandae aliquam. Blanditiis necessitatibus nulla dicta, odit voluptate doloremque, temporibus fugiat saepe nisi iusto, maiores eveniet officia!
+      Nobis doloribus molestias reiciendis voluptatum magni, neque maiores molestiae pariatur debitis consequatur similique aliquam nostrum fuga harum eum expedita quo aperiam. Ratione voluptatem sint rem. In totam aut ipsum odit?
+      Dolores architecto quod nisi cupiditate expedita quae corrupti voluptatum rem autem ratione, accusantium perspiciatis at sequi laboriosam, adipisci tempore sunt quos. Nihil voluptate est neque corrupti, doloribus quod? Saepe, ad.</p>
     </div>
   </main>
   <Footer></Footer>
